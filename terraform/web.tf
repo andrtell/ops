b484f6b546baa7160196ee0556c01df4611c3333
Andreas Tell <git@tell.nu>
2022-11-02 11:34:49 +0100

    removed load balancer

--- terraform/web.tf
+++ terraform/web.tf
@@ -11,38 +11,20 @@ resource "digitalocean_droplet" "web01" {
   ]
 }
 
-# web load-balancer
+# web network
 
-resource "digitalocean_loadbalancer" "public" {
-  name   = "loadbalancer01"
+resource "digitalocean_reserved_ip" "web01" {
   region = "ams3"
+}
 
-  vpc_uuid = digitalocean_vpc.tellnet.id
+resource "digitalocean_record" "web" {
+  domain = digitalocean_domain.tell-nu.id
+  type   = "A"
+  name   = "@"
+  value  = digitalocean_reserved_ip.web01.ip_address
+}
 
-  redirect_http_to_https = false
-
-  forwarding_rule {
-    entry_port     = 443
-    entry_protocol = "https"
-
-    target_port     = 443
-    target_protocol = "https"
-
-    tls_passthrough = true
-  }
-
-  forwarding_rule {
-    entry_port     = 80
-    entry_protocol = "http"
-
-    target_port     = 80
-    target_protocol = "http"
-  }
-
-  healthcheck {
-    port     = 22
-    protocol = "tcp"
-  }
-
-  droplet_ids = [digitalocean_droplet.web01.id]
+resource "digitalocean_reserved_ip_assignment" "web01" {
+  ip_address = digitalocean_reserved_ip.web01.ip_address
+  droplet_id = digitalocean_droplet.web01.id
 }
